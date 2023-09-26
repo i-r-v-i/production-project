7309fb549a2ad1a6e86ef564d4e7388cbd1156bf
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-09-26 17:31:17 +0300

    1 video

---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const webpack = require('webpack');
+import * as path from 'path';
+import * as webpack from 'webpack';
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
+
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'build'),
+      clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        }),
+        new webpack.ProgressPlugin(),
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+          },
+        ],
+      },
+      resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+      },
+  };
