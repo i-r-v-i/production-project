65293e18f554149b019e9db396db54332e38d7d5
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-09-30 00:42:42 +0300

    feat: video 1.4: add css loader

--- src/test.ts
+++
@@ -1,6 +0,0 @@
-export function someFn (arg: number): string {
-    console.log(arg, 'RANDOM opopopopoopopopo hhhhhhhhhhh')
-    return ''
-}
-
-// document.body.innerHTML =`<div style='background: red'>Hello World</div>`
