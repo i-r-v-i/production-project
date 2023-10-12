2b262980d957a18f57aa7032a07eaf397bbf4426
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-10-12 13:22:39 +0300

    feat: video 1.5: add cscc-module-plagin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,7 +1,7 @@
 import { BuildOptions } from './types/config';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import path from 'path';
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildPlugins ({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
     return [
@@ -9,5 +9,9 @@ export function buildPlugins ({paths}: BuildOptions): webpack.WebpackPluginInsta
             template: paths.html,
         }),
         new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthach:8].css'
+        })
     ]
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import './Counter.scss'
+import styles from './Counter.module.scss'
 
 
 export const Counter = () => {
@@ -8,7 +8,7 @@ export const Counter = () => {
     setCount(count + 1);
   };
   return (
-    <div>
+    <div className={styles.btn}> 
         <h1>{count}</h1>
       <button onClick={increment}>increment</button>
     </div>
