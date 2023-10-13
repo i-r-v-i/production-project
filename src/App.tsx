66cd7fe4b79b33d7bf403ebee87af350497a9ae2
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-10-13 11:13:58 +0300

    feat: video 1.7: Variables and thrmrs

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import "./styles/index.scss";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPageAsync";
+import { Suspense } from "react";
+
+import { useTheme } from "./theme/useTheme";
+
+const App = () => {
+const {theme, toogleTheme} = useTheme()
+  return (
+    <div className={`app ${theme}`}>
+      <button onClick={toogleTheme}>Change theme</button>
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
