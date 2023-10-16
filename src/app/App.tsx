1ab20f27f2a2cb98d1290b43a9327f979b69913b
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-10-16 14:01:17 +0300

    feat: video 1.12-Navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,16 @@
 import "./styles/index.scss";
-import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider/ui";
 import { AppRouter } from "./providers/router";
-
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
-const {theme, toogleTheme} = useTheme()
+  const { theme, toogleTheme } = useTheme();
   return (
-    <div className={classNames('app', {hovered: true, selected: true}, [theme])}>
+    <div className={classNames("app", { hovered: true, selected: true }, [theme])}>
+      <Navbar />
+      <AppRouter />
       <button onClick={toogleTheme}>Change theme</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
-      <AppRouter/>
     </div>
   );
 };
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -3,9 +3,9 @@ type Mods = Record<string, boolean | string>
 //     'hovered' : false
 // }
 
-export function classNames (cls: string, mods: Mods, additional: string[]): string {
+export function classNames (cls: string, mods: Mods = {}, additional: string[] = []): string {
 return [cls, 
-    ...additional,
+    ...additional.filter(Boolean),
     ...Object.entries(mods).filter(([className, value]) => Boolean(value)).map(([className]) => className)
  ].join(' ');
 }
