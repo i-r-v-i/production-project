65bb1bbfedddfda250ac226e0db5254b08625cfd
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-11-05 19:25:09 +0300

    feat: apply BundleAnalyzerPlugin

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,11 +1,14 @@
-import { type BuildOptions } from './types/config'
-import type { Configuration as DevServerConfigutation } from 'webpack-dev-server'
+import type { Configuration as DevServerConfigutation } from 'webpack-dev-server';
+import { type BuildOptions } from './types/config';
 
-export function buildDevServer (options: BuildOptions): DevServerConfigutation {
-  return {
-    port: options.port,
-    open: true,
-    historyApiFallback: true, // чтобы роутинг работал через главную страницу и не вылезала ошибка
-    hot: true
-  }
+export function buildDevServer(options: BuildOptions): DevServerConfigutation {
+    return {
+        port: options.port,
+        open: true,
+        historyApiFallback: true, // чтобы роутинг работал через главную страницу и не вылезала ошибка
+        hot: true,
+        client: {
+            overlay: false,
+        },
+    };
 }
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,10 @@
-import { BugButton } from 'app/providers/ErrorBoundary';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
     const { t } = useTranslation();
     return (
         <div>
-            <BugButton />
+            {/* <BugButton /> */}
             {t('Главная страница')}
         </div>
     );
