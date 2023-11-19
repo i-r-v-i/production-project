8efbb8f29bea79a98481c305e862abde63b04c3f
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-11-19 15:48:16 +0300

    feat: add storybook config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider/ui';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Suspense, useEffect } from 'react';
+import { Suspense } from 'react';
 import PageLoader from 'shared/ui/PageLoader/PageLoader';
 import { AppRouter } from './providers/router';
 
