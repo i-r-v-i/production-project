f1f32688082aa53a0f1b0b54038b3bdc0d744cb6
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-11-19 16:07:52 +0300

    feat: add webpack.config for storybook

---
+++ config/storybook/webpack.config.ts
@@ -0,0 +1,15 @@
+import webpack from 'webpack';
+import path from 'path';
+import { BuildPaths } from '../build/types/config';
+
+export default ({ config }: {config: webpack.Configuration}) => {
+    const paths: BuildPaths = {
+        entry: '',
+        build: '',
+        html: '',
+        src: path.resolve(__dirname, '..', '..', 'src'),
+    };
+    config.resolve?.modules?.push(paths.src);
+    config.resolve?.extensions?.push('ts', 'tsx');
+    return config;
+};
