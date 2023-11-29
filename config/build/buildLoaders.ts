3195d912236f9a9804b6d2dca1fa602294aea751
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-11-29 19:07:34 +0300

    feat: change webpack.config for storybook

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import type webpack from 'webpack';
 import { type BuildOptions } from './types/config';
+import { buildCssLoader } from './loaders/buildCssLoader';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
@@ -28,25 +28,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         },
     };
 
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module.scss')),
-                        localIdentName: isDev
-                            ? '[path][name]__[local]--[hash:base64:5]'
-                            : '[hash:base64:8]',
-                    },
-
-                },
-            },
-            'sass-loader',
-        ],
-    };
+    const cssLoader = buildCssLoader(isDev);
     // Если не используем тайпскрипт - нужен babel-loader
     const typescriptLoader = {
         test: /\.tsx?$/,
--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,6 +1,7 @@
 import webpack from 'webpack';
 import path from 'path';
 import { BuildPaths } from '../build/types/config';
+import { buildCssLoader } from '../build/loaders/buildCssLoader';
 
 export default ({ config }: {config: webpack.Configuration}) => {
     const paths: BuildPaths = {
@@ -10,6 +11,8 @@ export default ({ config }: {config: webpack.Configuration}) => {
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
     config.resolve?.modules?.push(paths.src);
-    config.resolve?.extensions?.push('ts', 'tsx');
+    config.resolve?.extensions?.push('.ts', '.tsx');
+
+    config.module?.rules?.push(buildCssLoader(true));
     return config;
 };
