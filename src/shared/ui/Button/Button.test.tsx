d612c82b1e71822b690298df1902b4847170d2f3
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-12-07 17:26:57 +0300

    fix: ignore error in Button.test

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -4,10 +4,12 @@ import { Button, ThemeButton } from 'shared/ui/Button/Button';
 describe('render Button', () => {
     test('Test render', () => {
         // TODO this
+        // eslint-disable-next-line i18next/no-literal-string
         render(<Button>TEST</Button>);
         expect(screen.getByText('TEST')).toBeInTheDocument();
     });
     test('withtheme', () => {
+        // eslint-disable-next-line i18next/no-literal-string
         render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
         expect(screen.getByText('TEST')).toHaveClass('clear');
         screen.debug();
