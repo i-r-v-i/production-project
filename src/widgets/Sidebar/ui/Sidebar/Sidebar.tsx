c88f93ce6901a58fe1f389fef9a85c1d115a19a9
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-12-07 16:41:24 +0300

    fix: fix error in SideBar with i18

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
+import { Button } from 'shared/ui/Button/Button';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -15,7 +16,7 @@ export const Sidebar = ({ className = '' }: SidebarProps) => {
     };
     return (
         <div data-testid="sidebar" className={classNames(cls.sidebar, { [cls.collapsed]: collapsed }, [className])}>
-            <button data-testid="sidebar-toogle" type="button" onClick={onToogle}>toogle</button>
+            {/* <Button data-testid="sidebar-toogle" type="button" onClick={onToogle}>toogle</Button> */}
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
