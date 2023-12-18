2ad3afed7327d4591f7d3a01f1a923f90d6dcb9b
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-12-18 10:25:45 +0300

    feat: change Sidebar links

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,4 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
@@ -11,15 +10,7 @@ export const Navbar = ({ className = '' }: NavBarProps) => {
     const { t } = useTranslation();
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
-            <div className={classNames(cls.links)}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={classNames(cls.mainLink)}>
-                    {t('Главная')}
-                </AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
-                    {' '}
-                    {t('О сайте')}
-                </AppLink>
-            </div>
+            /
 
         </div>
     );
